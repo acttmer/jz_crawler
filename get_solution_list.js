347e7f2b67cd0d0c412a3d16d6bdaa572527291b
Aego Yu <aego.yu@gmail.com>
2018-02-24 11:45:36 +0800

    added headers

--- get_solution_list.js
+++ get_solution_list.js
@@ -26,7 +26,7 @@ async function getSolutions() {
                 suffix += '&is_highlight=true'
             }
 
-            await snooze(200)
+            await snooze(100)
 
             let language_tags = await request({
                 uri: 'http://www.jiuzhang.com/api/solution/' + problem.unique_name + '/get_language_tags/?from=cu' + suffix,
@@ -58,7 +58,7 @@ async function getSolutions() {
                     code: codedata.results[0].code
                 })
 
-                await snooze(200)
+                await snooze(100)
             }
             
             console.log(' - processed ' + language_tags.length + ' languages')
@@ -79,7 +79,7 @@ async function getSolutions() {
             console.log('*** Failed to process Problem {id=' + problem.id + '}')
         }
 
-        await snooze(200)
+        await snooze(100)
 
         /*if (counter % 43 == 0) {
             console.log('=====> We must have a rest ^_^ <=====')
