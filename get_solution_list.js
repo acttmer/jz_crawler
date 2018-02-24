a5b59c849bbbc12ab275892869bdb0b889cafd11
Aego Yu <aego.yu@gmail.com>
2018-02-24 11:59:55 +0800

    added start_from function

--- get_solution_list.js
+++ get_solution_list.js
@@ -4,21 +4,20 @@ const problems = require('./problem_list.json')
 
 const snooze = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-async function getSolutions() {
+async function getSolutions(start_from = 0) {
     let list = []
     let counter = 0
 
     console.log('Total number of problems: ' + problems.length)
 
-    for (let problem of problems) {
+    for (let i = start_from; i < problems.length; i++) {
+        let problem = problems[i]
+
         try {
             let highlight_tags = await request({
                 uri: 'http://www.jiuzhang.com/api/solution/' + problem.unique_name + '/get_highlight_tags/',
                 json: true,
-                timeout: 10000,
-                headers: {
-                    'Cookie': '__cfduid=d1e03dad38c7968597dd5c7115609ec2b1519364912; _ga=GA1.2.801675240.1519364916; _gid=GA1.2.177831493.1519364916; csrftoken=bQApIgDRNPtkRo3oXFTHwEmATXY94fHB; sessionid=vrvao0msd0yrw7b8r1m6yn2p6jmv3byf; _gat=1' + Math.random()
-                }
+                timeout: 10000
             })
 
             let suffix = ''
@@ -31,10 +30,7 @@ async function getSolutions() {
             let language_tags = await request({
                 uri: 'http://www.jiuzhang.com/api/solution/' + problem.unique_name + '/get_language_tags/?from=cu' + suffix,
                 json: true,
-                timeout: 10000,
-                headers: {
-                    'Cookie': '__cfduid=d1e03dad38c7968597dd5c7115609ec2b1519364912; _ga=GA1.2.801675240.1519364916; _gid=GA1.2.177831493.1519364916; csrftoken=bQApIgDRNPtkRo3oXFTHwEmATXY94fHB; sessionid=vrvao0msd0yrw7b8r1m6yn2p6jmv3byf; _gat=1' + Math.random()
-                }
+                timeout: 10000
             })
 
             console.log('Problem {Id=' + problem.id + '}')
@@ -47,10 +43,7 @@ async function getSolutions() {
                 let codedata = await request({
                     uri: 'http://www.jiuzhang.com/api/solution_code/get_code_list/?unique_name=' + problem.unique_name + final_suffix,
                     json: true,
-                    timeout: 10000,
-                    headers: {
-                        'Cookie': '__cfduid=d1e03dad38c7968597dd5c7115609ec2b1519364912; _ga=GA1.2.801675240.1519364916; _gid=GA1.2.177831493.1519364916; csrftoken=bQApIgDRNPtkRo3oXFTHwEmATXY94fHB; sessionid=vrvao0msd0yrw7b8r1m6yn2p6jmv3byf; _gat=1' + Math.random()
-                    }
+                    timeout: 10000
                 })
                 
                 solutions.push({
@@ -77,20 +70,27 @@ async function getSolutions() {
         } catch (e) {
             fs.writeFileSync('cache/__cache_solution_list', JSON.stringify(list), 'utf-8')
             console.log('*** Failed to process Problem {id=' + problem.id + '}')
+            console.log('*** At index ' + i)
         }
 
         await snooze(100)
 
-        /*if (counter % 43 == 0) {
+        if (counter % 20 == 0) {
             console.log('=====> We must have a rest ^_^ <=====')
             await snooze(75000)
-        }*/
+        }
     }
 
     return list
 }
 
 ~async function () {
-    let data = await getSolutions()
+    let start_from = 0
+
+    if(fs.existsSync('start_from')) {
+        start_from = fs.readFileSync('start_from', 'utf-8')
+    }
+
+    let data = await getSolutions(start_from)
     fs.writeFileSync('solution_list.json', JSON.stringify(data), 'utf-8')
 }()
